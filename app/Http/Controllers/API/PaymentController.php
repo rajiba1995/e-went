54d2abaf16c180c4344c4eb98e57051172498f69
karumtb <karumtb@gmail.com>
2025-07-22 12:15:26 +0530

    ipn fixation

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PaymentLog;
 use App\Models\Payment;
+use App\Models\PaymentItem;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\OrderMerchantNumber;
 
